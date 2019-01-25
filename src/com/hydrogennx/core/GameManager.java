a5a19677ccfa18d49a5282b835e24f5c0fa06271
Edward <hpro1@sv156-imac24.sylvaniaschools.local>
2019-01-25 11:13:29 -0500

    Things
    netowrk mana and lgo file

--- src/com/hydrogennx/core/GameManager.java
+++ src/com/hydrogennx/core/GameManager.java
@@ -1,5 +1,6 @@
 package com.hydrogennx.core;
 
+import com.hydrogennx.controller.SettingsMenu;
 import com.hydrogennx.controller.WindowController;
 import com.hydrogennx.core.javafx.ScreenFramework;
 import javafx.animation.AnimationTimer;
@@ -26,7 +27,9 @@ public class GameManager extends Application {
     Stage primaryStage;
     Scene primaryScene;
 
-    public static void main(String[] args) { launch(args); }
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     @Override
     public void start(Stage primaryStage) {
@@ -120,14 +123,13 @@ public class GameManager extends Application {
 
     long lastFramePrintMillies = 0;
     int frames = 0;
+
     protected void startLoop() {
 
         final long startNanoTime = System.nanoTime();
 
-        new AnimationTimer()
-        {
-            public void handle(long currentNanoTime)
-            {
+        new AnimationTimer() {
+            public void handle(long currentNanoTime) {
                 double t = (currentNanoTime - startNanoTime) / 1000000000.0;
 
                 if (getGameInstance() != null) {
@@ -135,9 +137,8 @@ public class GameManager extends Application {
                     getGameInstance().update(t);
                     frames++;
                 }
-                if(System.currentTimeMillis()-lastFramePrintMillies>1000)
-                {
-                    System.out.println("FPS: "+frames);
+                if (System.currentTimeMillis() - lastFramePrintMillies > 1000) {
+                    System.out.println("FPS: " + frames);
                     frames = 0;
                     lastFramePrintMillies = System.currentTimeMillis();
                 }
@@ -236,6 +237,25 @@ public class GameManager extends Application {
         menu = Menu.MAIN_MENU;
 
         setScreen(ScreenFramework.MAIN_MENU_ID);
+
+    }
+
+    public void writeToLogFile(String message) {
+        String fileName = "Log"; // File you want to write to (will overwrite file)
+        try {
+            File jarFile = new File(SettingsMenu.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
+            fileName = jarFile.getParent() + File.separator + fileName;  // File.separator is the same as a "/"
+
+            FileWriter fileWriter = new FileWriter(fileName);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(message + "\n");
+            bufferedWriter.close();
+            fileWriter.close();
+        } catch (IOException ex) {
+            System.out.println("Error writing to file '" + fileName + "'");
+        } catch (Exception ex) {
+            System.err.println(ex);
+        }
 
     }
 }
